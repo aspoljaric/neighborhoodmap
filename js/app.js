b4a48309f60aab062a24bc9868a7c90f134ec32e
Albert Spoljaric <albert.spoljaric@gmail.com>
2016-09-16 11:11:06 +1000

    feat: filter list implemented
    populates markers on map based on filter

--- js/app.js
+++ js/app.js
@@ -4,48 +4,56 @@ var neighbourhood = [
         addressLine2: 'Preston VIC 3072',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.736144, lng:145.00451499999997 }
     },
     {
         addressLine1: '80 Cochranes Rd',
         addressLine2: 'Moorabbin VIC 3189',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.9437494, lng:145.0694519 }
     },
     {
         addressLine1: '35 Ebden St',
         addressLine2: 'Moorabbin VIC 3189',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.940406, lng:145.06770800000004 }
     },
     {
         addressLine1: '87 Kingsway',
         addressLine2: 'Glen Waverley VIC 3150',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.880845, lng:145.16328499999997 }
     },
     {
         addressLine1: '88 Kingsway',
         addressLine2: 'Glen Waverley VIC 3150',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.88118, lng:145.16391299999998 }
     },
     {
         addressLine1: '25/27 Portman St',
         addressLine2: 'Oakleigh VIC 3166',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.899998, lng:145.08981700000004 }
     },
     {
         addressLine1: '402 Chapel St',
         addressLine2: 'South Yarra VIC 3141',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.8451966, lng:144.99473950000004}
     },
     {
         addressLine1: '413 Brunswick St',
         addressLine2: 'Fitzroy VIC 3065',
         addressLine3: '',
         addressLine4: '',
+        latLng: { lat:-37.79459, lng:144.978878 }
     }
 ];
 
@@ -56,6 +64,7 @@ var NeighbourhoodLocation = function (data) {
     this.addressLine2 = ko.observable(data.addressLine2);
     this.addressLine3 = ko.observable(data.addressLine3);
     this.addressLine4 = ko.observable(data.addressLine4);
+    this.latLng = ko.observable(data.latLng);
 
     this.fullAddress = ko.computed(function() {
         var address = '';
@@ -80,21 +89,31 @@ var NeighbourhoodLocation = function (data) {
 var ViewModel = function() {
     var self = this;
 
-    this.neigbourhoodList = ko.observableArray([]);
-
+    self.neigbourhoodList = ko.observableArray([]);
     neighbourhood.forEach(function(neighbourhoodItem){
         self.neigbourhoodList.push(new NeighbourhoodLocation(neighbourhoodItem));
     });
 
-    this.currentNeighbourhoodLocation = ko.observable(this.neigbourhoodList()[0]);
+    self.filter =  ko.observable("");
 
-    // this.incrementCounter = function() {
-    //     self.currentCat().clickCount(self.currentCat().clickCount() + 1);
-    // };
+    self.filteredNeigbourhoodList = ko.computed(function()
+    {
+        clearMarkers();
+        return ko.utils.arrayFilter(self.neigbourhoodList(), function(item)
+        {
+            if (item.addressLine1().toLowerCase().indexOf(self.filter().toLowerCase()) !== -1)
+            {
+                addMarkerToGoogleMap(item.fullAddress(), false);
+                centreMap();
+            }
+            return item.addressLine1().toLowerCase().indexOf(self.filter().toLowerCase()) !== -1;
+        });
+
+    }, self).extend({ throttle: 500 });
+
 
     self.setNeighbourhoodLocation = function(clickedNeighbourhoodLocation) {
-        self.currentNeighbourhoodLocation(clickedNeighbourhoodLocation);
-        openInfoWindowByAddress(clickedNeighbourhoodLocation.fullAddress());
+        openInfoWindowByAddress(clickedNeighbourhoodLocation);
     };
 
 }
--- js/google-api.js
+++ js/google-api.js
@@ -4,7 +4,7 @@ var map;
 var markers = [];
 var mapCenter = {lat: -37.8203576, lng: 144.9516255}; // Melbourne, Australia
 var infowindow;
-
+var currentMarker;
 
 
 function initGoogleMap() {
@@ -15,24 +15,19 @@ function initGoogleMap() {
   zoom: 11,
   center: mapCenter
   });
+}
 
-  var neighborhood = ['456 High St, Preston VIC 3072',
-                          '80 Cochranes Rd, Moorabbin VIC 3189',
-                          '35 Ebden St, Moorabbin VIC 3189',
-                          '87 Kingsway, Glen Waverley VIC 3150',
-                          '88 Kingsway, Glen Waverley VIC 3150',
-                          '25/27 Portman St, Oakleigh VIC 3166',
-                          '402 Chapel St, South Yarra VIC 3141',
-                          '413 Brunswick St, Fitzroy VIC 3065'];
 
-  for (var i = 0; i < neighborhood.length; i++) {
-    addMarkerToGoogleMap(neighborhood[i], false);
+function centreMap() {
+  if(map != undefined) {
+    map.setZoom(11);
+    map.setCenter(mapCenter);
   }
 }
 
 
-
 function closeInfoWindow(marker) {
+  toggleMarker(marker);
   marker.setAnimation(null);
   map.setZoom(11);
   map.setCenter(mapCenter);
@@ -56,7 +51,7 @@ function createMarkerByGeoCoordinates(latLng) {
     animation: google.maps.Animation.DROP
   });
 
-  marker.addListener('click', openInfoWindowByMarker.bind(null, marker), false);
+  marker.addListener('click', openInfoWindowByMarker.bind(map, marker), false);
   markers.push(marker);
   return marker;
 }
@@ -64,17 +59,18 @@ function createMarkerByGeoCoordinates(latLng) {
 
 
 function toggleMarker(marker) {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-    map.setZoom(11);
-    map.setCenter(mapCenter);
-  }
-  else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    google.maps.event.addListener(infowindow,'closeclick', closeInfoWindow.bind(null, marker), false);
-    infowindow.open(map, marker);
-    map.setZoom(15);
-    map.setCenter(marker.getPosition());
+  if(marker != undefined) {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+      map.setZoom(11);
+      map.setCenter(mapCenter);
+    }
+    else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      google.maps.event.addListener(infowindow,'closeclick', closeInfoWindow.bind(null, marker), false);
+      map.setZoom(15);
+      map.setCenter(marker.getPosition());
+    }
   }
 }
 
@@ -126,32 +122,55 @@ function populateInfoWindowContent(businessName, phoneNumber, ratingImg, imgUrl,
 
 
 
-function reverseGeolocation(marker) {
+function reverseGeolocation(marker, processesGeolocation) {
   var geocoder = new google.maps.Geocoder;
-  var address;
   var latlng = marker.position;
 
   geocoder.geocode({
     latLng: latlng
   }, function(responses) {
-    if (responses && responses.length > 0) {
-      address = responses[0].formatted_address;
-      openInfoWindowByAddress(address);
-    }
+    processesGeolocation(responses);
   });
 }
 
 
 
 function openInfoWindowByMarker(marker) {
-  reverseGeolocation(marker);
+  infowindow.setContent('');
+  toggleMarker(currentMarker);
+  var formattedAddress;
+  reverseGeolocation(marker,function(address){
+     formattedAddress = address[0].formatted_address;
+     populateInfoWindowFromYelp(formattedAddress);
+     toggleMarker(marker);
+     currentMarker = marker;
+     infowindow.open(map, marker);
+  });
 }
 
 
 
 function openInfoWindowByAddress(address) {
   infowindow.setContent('');
-  clearMarkers();
+  toggleMarker(currentMarker);
+  var marker;
+  for(var i = 0; i < markers.length; i++) {
+    var lat = markers[i].position.lat();
+    var lng = markers[i].position.lng();
+
+    if(address.latLng().lat == lat && address.latLng().lng == lng) {
+        marker = markers[i];
+    }
+  }
+
+  getYelpInformationByLocation(address.fullAddress());
+  toggleMarker(marker);
+  currentMarker = marker;
+  infowindow.open(map, marker);
+}
+
+
+
+function populateInfoWindowFromYelp(address) {
   getYelpInformationByLocation(address);
-  addMarkerToGoogleMap(address, true);
 }
